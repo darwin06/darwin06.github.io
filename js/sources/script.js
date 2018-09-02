1eedb7ba3dbf80cc47dcfa5ca42106a3b894b1ed
darwin.mateos <darwin.mateos@scouting.org>
2018-09-02 11:59:24 -0500

    Grid Version updated

--- js/sources/script.js
+++ js/sources/script.js
@@ -9555,5 +9555,35 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
 })( window );
 
 $(document).ready( function () {
-  console.info('hola');
+  // * Change Year in footer
+  var d = new Date();
+  var n = d.getFullYear('Y');
+  var ftYear = document.getElementById('year');
+  ftYear.innerText = n;
+
+  // * Smooth scroll
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
+  // * Scroll top
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
 });
--- js/sources/scripts.js
+++ js/sources/scripts.js
@@ -1,3 +1,33 @@
 $(document).ready( function () {
-  console.info('hola Darwin');
+  // * Change Year in footer
+  var d = new Date();
+  var n = d.getFullYear('Y');
+  var ftYear = document.getElementById('year');
+  ftYear.innerText = n;
+
+  // * Smooth scroll
+  $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+
+  // * Scroll top
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollToTop').fadeIn();
+    } else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollToTop').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
 });
